14dbda355502f132ec9d933f36162df3aa7d32d3
alexesca <alexescamore@gmail.com>
2020-10-26 15:42:44 -0600

    changes for deployment to Heroku

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static'; // <- INSERT LINE
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'nx-app'),
+      exclude: ['/api*']
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
