a9e84496fad316eb0bf0a52e53d220ade3f0477c
alexesca <alexescamore@gmail.com>
2020-10-26 20:34:37 -0600

    Added auth0 to angular app

--- apps/nx-app/src/app/app.module.ts
+++ apps/nx-app/src/app/app.module.ts
@@ -4,10 +4,35 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
+import { AuthModule } from '@auth0/auth0-angular';
+import { environment as env } from '../environments/environment';
+
+import { AuthNavComponent } from './components/auth-nav/auth-nav.component';
+import { AuthenticationButtonComponent } from './components/authentication-button/authentication-button.component';
+import { LoginButtonComponent } from './components/login-button/login-button.component';
+import { LogoutButtonComponent } from './components/logout-button/logout-button.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { SignupButtonComponent } from './components/signup-button/signup-button.component';
+
+
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    AuthNavComponent,
+    AuthenticationButtonComponent,
+    LoginButtonComponent,
+    LogoutButtonComponent,
+    NavBarComponent,
+    SignupButtonComponent
+  ],
+  imports: [
+    BrowserModule, HttpClientModule,
+    AuthModule.forRoot({
+      ...env.auth,
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
