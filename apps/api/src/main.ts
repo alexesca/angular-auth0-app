9c4548f6b30b360434be7dc87deb07779c4b3ccb
alexesca <alexescamore@gmail.com>
2020-10-29 10:26:16 -0600

    Added middleware to test app

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,9 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as jwt from "express-jwt";
+import * as jwks from "jwks-rsa";
+const url = require('url');
 
 import { AppModule } from './app/app.module';
 
@@ -12,6 +15,30 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+
+  const auth0JWTMiddleware = jwt({
+    secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: process.env.JWKS_URI
+    }),
+    audience: process.env.AUTH0_AUDIENCE,
+    issuer: process.env.AUTH0_ISSUER_URL,
+    algorithms: [process.env.AUTH_ALGORITHM]
+  })
+    .unless({
+      path: [
+        '/index.html',
+        { url: '/', methods: ['GET', 'PUT'] }
+      ],
+      custom: function (req) {
+        var ext = url.parse(req.originalUrl).pathname.substr(-4);
+        return !~['.jpg', '.html', '.css', '.js', '.json', '.png', '.ico'].indexOf(ext);
+      }
+    })
+  app.use(auth0JWTMiddleware);
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
